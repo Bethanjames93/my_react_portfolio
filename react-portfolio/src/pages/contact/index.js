ac40cd2e1eaac00517699b9cd87514575f71233e
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2020-03-29 23:22:22 +0100

    updated links on the contact page

--- react-portfolio/src/pages/contact/index.js
+++ react-portfolio/src/pages/contact/index.js
@@ -5,27 +5,21 @@ function Contact() {
     return(
         <div className="container">
             <div className="row">
-                <div className="col-12"><h2>Contact</h2></div>
+                <div className="col-12"><h2>Contact Me</h2></div>
+                <ul>
+                    <li><a href="https://github.com/Bethanjames93">GitHub</a></li>
+                    <li><a href="www.linkedin.com/in/bethanjames">LinkedIn</a></li>
+                    <li><a href="bethanjames93@icloud.com">bethanjames93@icloud.com</a></li>
+                </ul>
             </div>
-            <div className="row">
-                <div class="col-12">
-                    <h3>My Pages</h3>
-                    <a href="https://github.com/Bethanjames93">GitHub</a>
-                    <a href="www.linkedin.com/in/bethanjames">LinkedIn</a>
-                </div>
-            </div>
+    
             <div className="row">
                 <div class="col-12">
                     <h3>My CV</h3>
-                    <a href="assets/resume.pdf">CV</a>
+                    <p>Heres a PDF link to my <a href="assets/resume.pdf">CV</a></p>
                 </div>
             </div>
-            <div className="row">
-                <div class="col-12">
-                    <h3>My Email</h3>
-                    <a href="bethanjames93@icloud.com">bethanjames93@icloud.com</a>
-                </div>
-            </div>
+  
         </div>
 
     );
