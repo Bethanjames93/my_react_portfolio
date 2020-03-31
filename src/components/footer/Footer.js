df25e0b06400daefa76b52b9261423a1ab5baea8
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2020-03-31 12:29:35 +0100

    moved files into the root folder to fix heroku build

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css"
+
+function Footer() {
+    return (
+        <nav className="navbar fixed-bottom footer">Copyright &#9400;</nav>
+    );
+}
+
+export default Footer
---
+++ src/pages/about/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css"
+
+function About() {
+    return(
+        <div className="container">
+            <div className="row">
+                <div class="col-5" src="Assets/imageofme.jpg" title="Image of me"></div>
+                <div class="col-7">
+                    <h2>Welcome to my Portfolio</h2>
+                    <p>My name is Bethan James, I am currently studying at Manchester University taking part in an intense 24 week bootcamp.</p>
+                    <p>I hope you enjoy looking through my portfolio! If you visit the Projects page you can take a look at some of the things I have created during my time at the bootcamp</p>
+                    <p>Take a look at my CV, GitHub and LinkedIn account on my contact page.</p>
+                    <p>I am currently looking for a position in Manchester</p>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default About;
