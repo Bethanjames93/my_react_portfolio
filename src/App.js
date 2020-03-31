4ef87bd4eb39cdc8dddec77c102036bd2be9ed08
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2020-03-31 16:36:33 +0100

    moved pages folder into conponents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import About from "./pages/about";
-import Projects from "./pages/projects";
-import Contact from "./pages/contact";
+import About from "./components/pages/about";
+import Projects from "./components/pages/projects";
+import Contact from "./components/pages/contact";
 import Navbar from "./components/navbar/Navbar.js";
 import Footer from "./components/footer/Footer.js";
 import './App.css';
