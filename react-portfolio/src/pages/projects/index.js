ef721165dd3216030421c266743ebbe4ec3c609d
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2020-03-31 11:42:06 +0100

    added the first row of projects with links to the github repos

---
+++ react-portfolio/src/pages/projects/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./style.css"
+
+function Projects() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12"><h2>My Projects</h2></div>
+
+            <div className="row"> 
+                <div className="col-4">
+                    <div className="card">
+                        <div class="card-body">
+                            <h4>Living in the Scene</h4>
+                            <p><a href="https://github.com/beenish-mha/Living-in-the-scene">GitHub Repo</a></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-4">
+                    <div className="card">
+                        <div class="card-body">
+                            <h4>DripAdvisor</h4>
+                            <p><a href="https://github.com/Bethanjames93/bootcamp_beeefcakes">GitHub Repo</a></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-4">
+                    <div className="card">
+                        <div class="card-body">
+                            <h4>Eat da burger</h4>
+                            <p><a href="https://github.com/Bethanjames93/eat_da_burger">GitHub Repo</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            </div>
+        </div>
+    );
+}
+
+export default Projects
